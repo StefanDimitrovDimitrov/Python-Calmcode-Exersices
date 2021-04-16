70e5393e3a7f7b472e97509934333a7a26cfa605
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-04-16 07:15:11 +0300

    black

---
+++ black.py
@@ -0,0 +1,47 @@
+"""
+
+Black is the uncompromising Python code formatter.
+By using it, you agree to cede control over minutiae of hand-formatting.
+In return, Black gives you speed, determinism,
+and freedom from pycodestyle nagging about formatting.
+You will save time and mental energy for more important matters.
+
+Installation
+Black can be installed by running
+
+        pip install black
+
+It requires Python 3.6.0+ to run but you can reformat Python 2 code with it, too.
+"""
+"""
+Usage
+To get started right away with sensible defaults:
+
+black {source_file_or_directory}
+
+"""
+
+list_of_numbers = [1, 2, 3, 4, 5]
+
+large_dictionary = {
+    1: 2,
+    2: 2,
+    3: 2,
+    4: 2,
+    5: 2,
+    6: 2,
+    7: 2,
+    8: 2,
+    9: 2,
+    10: 2,
+    11: 2,
+    12: 2,
+    13: 2,
+    14: 2,
+    15: 2,
+    16: 2,
+    17: 2,
+    18: 2,
+    19: 2,
+    20: 2,
+}
